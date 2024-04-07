e8b5f918590864bdd70c97021c55bcede22feb31
Torrin <38574537+torrinjbrown@users.noreply.github.com>
2024-04-07 10:54:15 -0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,18 @@
+
+#Importing from the config.py which is going to be the database
+from config import db
+
+#Creating a class to capture the contact of the user 
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "firstName": self.first_name,
+            "lastName": self.last_name,
+            "email": self.email, 
+        }
